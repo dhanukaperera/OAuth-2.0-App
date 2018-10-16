38787fcc96cbc24300c74f8d595f09a45a10789f
Dhanuka Perera <dhanukashehanperera@gmail.com>
2018-10-16 10:47:44 +0530

    loading repos

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for,render_template,make_response
 from flask_dance.contrib.github import make_github_blueprint, github
 import os 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
@@ -10,7 +10,24 @@ github_blueprint = make_github_blueprint(client_id='5b8003c3f9728acb0501', clien
 
 app.register_blueprint(github_blueprint, url_prefix='/github_login')
 
+user = {
+    "name" : '',
+    "login" :'',
+    "avatar_url":''
+}
 
+repos = []
+
+
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/profile')
+def profile():    
+    print(repos)
+    return render_template('profile.html',user= user,repos=repos)
 
 @app.route('/github')
 def github_login():
@@ -18,10 +35,29 @@ def github_login():
         return redirect(url_for('github.login'))
 
     account_info = github.get('/user')
+    repo_info = github.get('/user/repos')
 
     if account_info.ok:
         account_info_json = account_info.json()
-        return '<h1>Your Github name is {} '.format(account_info_json['login'] )
+        repo_info_json = repo_info.json()
+        print(len(repo_info_json))
+       
+        for r in repo_info_json :
+            
+            repos.append({"name":r.get('name')})
+       
+
+        user['name'] = account_info_json.get('name')
+        user['login'] = account_info_json.get('login')
+        user['avatar_url'] = account_info_json.get('avatar_url')
+
+
+        return redirect(url_for('profile'))
+
+        #redirect(url_for('github'))
+        #return '<h1>Your Github name is {} '.format(account_info_json['login'] )
+        #return render_template('github.html',name=account_info.get('name'))
+
 
     return redirect(url_for('github.login'))
 
